f968ac9e4cbe9cb39e819795ea668683df329848
AbyxDev <ken@abyx.dev>
2022-01-05 07:51:17 -0500

    Add regional indicator characters

--- AbyxBot/chars.py
+++ AbyxBot/chars.py
@@ -2,3 +2,8 @@ ZWNJ = '\N{ZERO WIDTH NON-JOINER}'
 LABR = '\N{MATHEMATICAL LEFT ANGLE BRACKET}'
 RABR = '\N{MATHEMATICAL RIGHT ANGLE BRACKET}'
 DONE = '\N{WHITE HEAVY CHECK MARK}'
+
+# regional indicators, for use in flags
+REGI = tuple(chr(0x1f1e6 + i) for i in range(26)) # indexes
+REGU = {chr(0x41 + i): reg for i, reg in enumerate(REGI)} # uppercase letters
+REGL = {letter.lower(): reg for letter, reg in REGU.items()} # lowercase ''
