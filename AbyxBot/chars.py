3f2631b61ca367ae75094696969e294727e77363
AbyxDev <ken@abyx.dev>
2022-01-16 02:04:18 -0500

    Add new arrow emojis

--- AbyxBot/chars.py
+++ AbyxBot/chars.py
@@ -7,3 +7,8 @@ DONE = '\N{WHITE HEAVY CHECK MARK}'
 REGI = tuple(chr(0x1f1e6 + i) for i in range(26)) # indexes
 REGU = {chr(0x41 + i): reg for i, reg in enumerate(REGI)} # uppercase letters
 REGL = {letter.lower(): reg for letter, reg in REGU.items()} # lowercase ''
+
+UP = '<:up:932162569731473438>'
+DOWN = '<:down:932162569790193695>'
+LEFT = '<:left:932162569530126388>'
+RIGHT = '<:right:932162569362358293>'
