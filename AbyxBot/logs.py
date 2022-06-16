09791663113467cd489eb0ddea2c525c5a7d9f99
AbyxDev <ken@abyx.dev>
2022-06-15 22:53:20 -0700

    Fix typing errors in logs

--- AbyxBot/logs.py
+++ AbyxBot/logs.py
@@ -16,19 +16,19 @@ os.makedirs('logs', exist_ok=True)
 
 def activate():
     queue = asyncio.Queue()
-    handler = QueueHandler(queue)
+    handler = QueueHandler(queue) # type: ignore - still implements put_nowait
     handler.setFormatter(logging.Formatter(FORMAT, style='{'))
     logging.basicConfig(handlers=[handler], level=logging.WARNING)
     logging.getLogger('AbyxBot').setLevel(cmdargs.level)
     logging.getLogger('discord').setLevel(logging.INFO)
     return asyncio.create_task(consume_logs(queue))
 
-async def consume_logs(queue: asyncio.Queue):
+async def consume_logs(queue: asyncio.Queue[logging.LogRecord]):
     if cmdargs.stdout:
         handler = logging.StreamHandler(sys.stdout)
     else:
         handler = logging.FileHandler(
             'logs/%s.log' % time.strftime(TIME), 'a', 'utf8')
     while 1: # the nice thing about tasks is that they can be cancelled
-        record: logging.LogRecord = await queue.get()
+        record = await queue.get()
         handler.emit(record)
