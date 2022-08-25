1be3ddcd2cc2840184a411565ceaa5b1f0194b89
AbyxDev <ken@abyx.dev>
2022-08-24 22:45:05 -0700

    Add /sudo eval using modal (!!!)

--- AbyxBot/sudo.py
+++ AbyxBot/sudo.py
@@ -1,6 +1,10 @@
 # stdlib
+import re
+from io import StringIO
+from contextlib import redirect_stdout
+import traceback
 import asyncio
-from typing import cast
+from typing import Any, Awaitable, Callable, cast
 
 # 3rd-party
 import discord
@@ -10,6 +14,81 @@ from discord import app_commands
 # 1st-party
 from .i18n import Msg, error_embed, mkmsg
 from .utils import asyncify
+
+# eval-related stuff
+
+def _init_env(ctx: discord.Interaction) -> tuple[dict[str, Any], StringIO]:
+    def exit(*args):
+        raise SystemExit(*args)
+    env = {
+        'asyncio': asyncio,
+        'ctx': ctx,
+        'commands': commands,
+        'discord': discord,
+        'exit': exit,
+    }
+    out = StringIO()
+    return env, out
+
+def _func_from_arg(arg: str, env: dict[str, Any]
+                   ) -> tuple[str, Callable[[], Awaitable[Any]]]:
+    lines = arg.strip('`').rstrip().lstrip('\n').splitlines()
+    indent = '    '
+    for line in lines:
+        strp = line.lstrip()
+        if strp != line:
+            indent = line[:len(line) - len(strp)]
+            break
+    lines = ''.join(indent * 2 + line + '\n' for line in lines)
+    globline = ', '.join(i for i in env.keys() if i not in {
+        'func', 'asyncio', 'commands', 'discord', 'exit'
+    })
+    lines = f'''{indent}try:
+{indent*2}global {globline}
+{lines}
+{indent}finally:
+{indent*2}globals().update(locals())'''
+    lines = f"async def func():\n{lines}"
+    exec(lines, env)
+    return lines, env['func']
+
+def _trace():
+    return re.sub(r'(?i)".*?abyxbot[\\/]', '"', traceback.format_exc())
+
+class EvalModal(discord.ui.Modal):
+
+    def __init__(self, ctx: discord.Interaction) -> None:
+        super().__init__(title=mkmsg(ctx, 'sudo/eval-title'))
+        self.text = discord.ui.TextInput(
+            label=mkmsg(ctx, 'sudo/code-label'),
+            style=discord.TextStyle.paragraph,
+            required=True
+        )
+        self.add_item(self.text)
+
+    async def on_submit(self, ctx: discord.Interaction) -> None:
+        await ctx.response.defer(thinking=True)
+        env, out = _init_env(ctx)
+        text, func = _func_from_arg(self.text.value, env)
+        kwargs = {}
+        try:
+            with redirect_stdout(out):
+                ret = await func()
+        except SystemExit:
+            pass
+        except BaseException:
+            trace = _trace()
+            kwargs['embed'] = error_embed(
+                ctx, f'```\n{trace}\n```\n`func`:```\n{text}\n```')
+        else:
+            kwargs['embed'] = discord.Embed(
+                title=mkmsg(ctx, 'success'),
+                description=f'```{ret!r}```',
+                color=discord.Color.blue())
+        kwargs['content'] = f'```\n{out.getvalue()}\n```'
+        await ctx.edit_original_response(**kwargs)
+
+# actual cog
 
 @app_commands.default_permissions()
 class Sudo(app_commands.Group):
@@ -47,6 +126,11 @@ class Sudo(app_commands.Group):
         await bot.tree.sync()
         await ctx.response.send_message(
             mkmsg(ctx, 'sudo/sync'), ephemeral=True)
+
+    @app_commands.command()
+    async def eval(self, ctx: discord.Interaction):
+        """Evaluate arbitrary code."""
+        await ctx.response.send_modal(EvalModal(ctx))
 
 def setup(bot: commands.Bot):
     bot.tree.add_command(Sudo())
