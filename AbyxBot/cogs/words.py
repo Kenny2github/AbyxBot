f7b744bba6da4d48d8dcf58c380838384bf28e98
AbyxDev <ken@abyx.dev>
2025-04-23 17:24:33 -0400

    /words as parameters, not subcommands

--- AbyxBot/cogs/words.py
+++ AbyxBot/cogs/words.py
@@ -1,6 +1,5 @@
 # stdlib
-from functools import wraps
-from typing import Callable, TypedDict
+from typing import TypedDict
 from operator import itemgetter
 from itertools import groupby
 
@@ -17,24 +16,7 @@ from ..i18n import Msg, mkembed, mkmsg, error_embed
 
 session: aiohttp.ClientSession = None # type: ignore - set on init
 
-TITLES = {
-    'ml': 'words/meaning-title',
-    'sl': 'words/sounding-like-title',
-    'sp': 'words/spelled-like-title',
-    'rel_jja': 'words/modified-by-title',
-    'rel_jjb': 'words/modifying-title',
-    'rel_syn': 'words/synonyms-title',
-    'rel_trg': 'words/associated-with-title',
-    'rel_ant': 'words/antonyms-title',
-    'rel_spc': 'words/hypernyms-title',
-    'rel_gen': 'words/hyponyms-title',
-    'rel_com': 'words/holonyms-title',
-    'rel_par': 'words/meronyms-title',
-    'rel_bga': 'words/after-title',
-    'rel_bgb': 'words/before-title',
-    'rel_hom': 'words/homophones-title',
-    'rel_cns': 'words/consonant-match-title',
-}
+API_URL = 'https://api.datamuse.com/words'
 
 class WordResp(TypedDict):
     score: int
@@ -46,53 +28,100 @@ class WordResp(TypedDict):
 
 async def fetch_words(**kwargs) -> list[WordResp]:
     """Get API data, passing kwargs."""
-    API_URL = 'https://api.datamuse.com/words'
     async with session.get(API_URL, params=kwargs) as resp:
         return await resp.json()
 
-def format_words(ctx: discord.Interaction, api_code: str, word: str,
-                 data: list[WordResp]) -> discord.Embed:
-    """Format API data into an embed."""
-    return mkembed(ctx,
-        title=Msg(TITLES[api_code], word),
+@app_commands.command()
+@app_commands.describe(
+    meaning='[ml] Words that mean this.',
+    sounding_like='[sl] Words that sound like this.',
+    spelled_like='[sp] Words that are spelled like this - wildcards allowed: '
+    'https://onelook.com/thesaurus/#patterns',
+    modified_by='[rel_jja] Popular nouns modified by this adjective, per '
+    'Google Books Ngrams.',
+    modifying='[rel_jjb] Popular adjectives used to modify this noun, per '
+    'Google Books Ngrams.',
+    associated_with='[rel_trg] Words that are statistically associated with '
+    'this word in the same piece of text.',
+    after='[rel_bga] Frequent predecessors of this word (per Google Books '
+    'Ngrams).',
+    before='[rel_bgb] Words that frequently follow this word (per Google '
+    'Books Ngrams).',
+    synonymizing='[rel_syn] Synonyms of this word (per WordNet).',
+    antonymizing='[rel_ant] Antonyms of this word (per WordNet).',
+    hypernymizing='[rel_spc] Hypernyms (words that are more general than this '
+    'word, per WordNet).',
+    hyponymizing='[rel_gen] Hyponyms (words that are more specific than this '
+    'word, per WordNet).',
+    holonymizing='[rel_com] Holonyms (words that are a part of this word, per '
+    'WordNet).',
+    meronymizing='[rel_par] Meronyms (words that this word is part of, per '
+    'WordNet).',
+    homophonizing='[rel_hom] Homophones (words that sound alike).',
+    consonantizing='[rel_cns] Words that match consonants.',
+    topics='Bias words related to these words to appear first.'
+)
+async def words(
+    ctx: discord.Interaction,
+    meaning: str | None = None,
+    sounding_like: str | None = None,
+    spelled_like: str | None = None,
+    modified_by: str | None = None,
+    modifying: str | None = None,
+    associated_with: str | None = None,
+    after: str | None = None,
+    before: str | None = None,
+    synonymizing: str | None = None,
+    antonymizing: str | None = None,
+    hypernymizing: str | None = None,
+    hyponymizing: str | None = None,
+    holonymizing: str | None = None,
+    meronymizing: str | None = None,
+    homophonizing: str | None = None,
+    consonantizing: str | None = None,
+    topics: str | None = None,
+):
+    """Lexical data related to words! Parameters can be combined."""
+    kwargs = {
+        'ml': meaning,
+        'sl': sounding_like,
+        'sp': spelled_like,
+        'rel_jja': modified_by,
+        'rel_jjb': modifying,
+        'rel_trg': associated_with,
+        'rel_bga': after,
+        'rel_bgb': before,
+        'rel_syn': synonymizing,
+        'rel_ant': antonymizing,
+        'rel_spc': hypernymizing,
+        'rel_gen': hyponymizing,
+        'rel_com': holonymizing,
+        'rel_par': meronymizing,
+        'rel_hom': homophonizing,
+        'rel_cns': consonantizing,
+        'topics': topics,
+    }
+    kwargs = {key: value for key, value in kwargs.items() if value is not None}
+    if not kwargs:
+        await ctx.response.send_message(embed=error_embed(ctx, 'words/no-request'))
+        return
+    await ctx.response.defer()
+    data: list[WordResp] = await fetch_words(**kwargs)
+    await ctx.edit_original_response(embed=mkembed(ctx,
+        title=Msg('words/title'),
         description=mkmsg(ctx, ',').join(
             '{score}: {word}'.format(**i)
             for i in data
         ) or Msg('none-paren'),
         color=discord.Color.blue()
-    )
-
-def endpoint(group: app_commands.Group, api_code: str, param_desc: str):
-    """Take a stub function and make it into a subcommand
-    that returns the corresponding API data.
-    """
-    def decorator(func: Callable):
-        assert func.__doc__ is not None
-        param_name = [key for key, value in func.__annotations__.items()
-                      if value is str or value == 'str'][0]
-        @group.command(name=func.__name__, description=func.__doc__)
-        @app_commands.describe(**{param_name: param_desc})
-        @wraps(func)
-        async def subcommand(ctx: discord.Interaction, **kwargs):
-            word = kwargs[param_name]
-            await ctx.response.defer()
-            resp = await fetch_words(**{api_code: word})
-            await ctx.edit_original_response(
-                embed=format_words(ctx, api_code, word, resp))
-        return subcommand
-    return decorator
-
-### /words <command> <arg>:<thing>
-
-words = app_commands.Group(
-    name='words', description="""Lexical data related to words!""")
+    ))
 
 ### rhyming is a special case
 
-@words.command()
+@app_commands.command()
 @app_commands.rename(word='with')
 @app_commands.describe(word='The word that other words rhyme with.')
-async def rhyming(ctx: discord.Interaction, word: str):
+async def rhymes(ctx: discord.Interaction, word: str):
     """Words that rhyme with a word."""
     await ctx.response.defer()
     perf: list[WordResp] = await fetch_words(rel_rhy=word, md='s')
@@ -131,84 +160,8 @@ async def rhyming(ctx: discord.Interaction, word: str):
     )
     await ctx.edit_original_response(embeds=[perf_embed, near_embed])
 
-@endpoint(words, 'ml', 'A phrase.')
-async def meaning(ctx: discord.Interaction, phrase: str):
-    """Words that mean something."""
-
-@endpoint(words, 'sl', 'The sound.')
-async def sounding(ctx: discord.Interaction, like: str):
-    """Words that sound like something."""
-
-@endpoint(
-    words, 'sp',
-    'The spelling. Wildcards allowed - use `re*ing` to get words starting '
-    'with re- and ending with -ing')
-async def spelled(ctx: discord.Interaction, like: str):
-    """Words that are spelled like something."""
-
-@endpoint(words, 'rel_jja', 'An adjective.')
-async def modified(ctx: discord.Interaction, by: str):
-    """Popular nouns modified by the given adjective, \
-per Google Books Ngrams."""
-
-@endpoint(words, 'rel_jjb', 'A noun.')
-async def modifying(ctx: discord.Interaction, word: str):
-    """Popular adjectives used to modify the given noun, \
-per Google Books Ngrams."""
-
-@endpoint(words, 'rel_trg', 'A word.')
-async def associated(ctx: discord.Interaction, word: str):
-    """Words that are statistically associated with the query word \
-in the same piece of text."""
-
-@endpoint(words, 'rel_bga', 'A word.')
-async def after(ctx: discord.Interaction, word: str):
-    """Frequent predecessors of the given word (per Google Books Ngrams)."""
-
-@endpoint(words, 'rel_bgb', 'A word.')
-async def before(ctx: discord.Interaction, word: str):
-    """Words that frequently follow the given word \
-(per Google Books Ngrams)."""
-
-### /lex <command> <arg>:<word>
-
-lex = app_commands.Group(
-    name='lex', description="""Lexical data related to words!""")
-
-@endpoint(lex, 'rel_syn', 'A word.')
-async def synonyms(ctx: discord.Interaction, word: str):
-    """Synonyms of the given word (per WordNet)."""
-
-@endpoint(lex, 'rel_ant', 'A word.')
-async def antonyms(ctx: discord.Interaction, word: str):
-    """Antonyms of the given word (per WordNet)."""
-
-@endpoint(lex, 'rel_spc', 'A word.')
-async def hypernyms(ctx: discord.Interaction, word: str):
-    """Hypernyms (words that are more general than the word, per WordNet)."""
-
-@endpoint(lex, 'rel_gen', 'A word.')
-async def hyponyms(ctx: discord.Interaction, word: str):
-    """Hyponyms (words that are more specific than the word, per WordNet)."""
-
-@endpoint(lex, 'rel_com', 'A word.')
-async def holonyms(ctx: discord.Interaction, word: str):
-    """Holonyms (words that are a part of the word, per WordNet)."""
-
-@endpoint(lex, 'rel_par', 'A word.')
-async def meronyms(ctx: discord.Interaction, word: str):
-    """Meronyms (words that the word is part of, per WordNet)."""
-
-@endpoint(lex, 'rel_hom', 'A word.')
-async def homophones(ctx: discord.Interaction, word: str):
-    """Homophones (words that sound alike)."""
-
-@endpoint(lex, 'rel_cns', 'A word.')
-async def homoconsonants(ctx: discord.Interaction, word: str):
-    """Words that match consonants."""
-
 def setup(bot: commands.Bot):
     global session
     session = aiohttp.ClientSession()
-    for cmd in {words, lex}:
-        bot.tree.add_command(cmd)
+    bot.tree.add_command(words)
+    bot.tree.add_command(rhymes)
